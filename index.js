9217c98d605901c66a5a512ad9a55c1a7a359851
Sriram <santhanamsriram07@gmail.com>
2021-09-08 12:57:51 +0530

    fixed mistake in contact form

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
-        event.target.reset();
 
         // these IDs from the previous steps
         emailjs.sendForm('service_hhvtnlb', 'template_l7rw09g', this)
             .then(function() {
                 console.log('SUCCESS!');
                 displayToast("Success!!");
+                event.target.reset();
             }, function(error) {
                 console.log('FAILED...', error);
                 displayToast("Failed");
@@ -18,9 +18,9 @@ window.onload = function() {
 function displayToast(text) {
     let toast = document.querySelector('#snackbar');
     
-    toast.className = "show";
     toast.innerText = text;
+    toast.className = "show";
     setTimeout(() => {
-        toast.className = toast.className.replace('show', "");
+        toast.className = toast.className.replace('show', '');
     }, 3000);
 }
